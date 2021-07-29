708e1beac1942f1fa0e67400f68c4d2dcbd180c6
ArianaVillegas <ariana.villegas@utec.edu.pe>
2021-07-29 12:03:04 -0500

    Combine ast - nfa - dfa

--- functions.cpp
+++ functions.cpp
@@ -1,12 +1,86 @@
 #include <vector>
+#include <stack>
 #include <algorithm>
 #include <omp.h>
 #include <math.h>
-#include "af.h"
+#include "models/af.h"
+#include "models/Regx.cpp"
+#include "models/operators.cpp"
 
 #define n_threads 4
 
 using namespace std;
+
+AF re_to_nfa(string re)
+{
+    auto reg = RegExParser(re);
+    auto ast_nodes = reg.postorder();
+    stack<AF> nfa_nodes;
+    while (!ast_nodes.empty())
+    {
+        auto ast_node = ast_nodes.top();
+        ast_nodes.pop();
+
+        if (ast_node == "conc")
+        {
+            auto left_nfa = nfa_nodes.top();
+            nfa_nodes.pop();
+            auto right_nfa = nfa_nodes.top();
+            nfa_nodes.pop();
+
+            vector<AF> ops;
+            ops.push_back(left_nfa);
+            ops.push_back(right_nfa);
+
+            nfa_nodes.push(concat_operator(ops));
+        }
+        else if (ast_node == "*")
+        {
+            auto cur_nfa = nfa_nodes.top();
+            nfa_nodes.pop();
+            cur_nfa.add_transition(0, cur_nfa.get_size_vertexs() - 1, '^');
+            cur_nfa.add_transition(cur_nfa.get_size_vertexs() - 1, 0, '^');
+            nfa_nodes.push(cur_nfa);
+        }
+        else if (ast_node == "|")
+        {
+            auto left_nfa = nfa_nodes.top();
+            nfa_nodes.pop();
+            auto right_nfa = nfa_nodes.top();
+            nfa_nodes.pop();
+
+            vector<AF> ops;
+            ops.push_back(left_nfa);
+            ops.push_back(right_nfa);
+
+            nfa_nodes.push(or_operator(ops));
+        }
+        else if (ast_node == "+")
+        {
+            auto cur_nfa = nfa_nodes.top();
+            nfa_nodes.pop();
+            cur_nfa.add_transition(cur_nfa.get_size_vertexs() - 1, 0, '^');
+            nfa_nodes.push(cur_nfa);
+        }
+        else if (ast_node == "?")
+        {
+            auto cur_nfa = nfa_nodes.top();
+            nfa_nodes.pop();
+            cur_nfa.add_transition(0, cur_nfa.get_size_vertexs() - 1, '^');
+            nfa_nodes.push(cur_nfa);
+        }
+        else
+        {
+            AF *nfa_node = new AF();
+            nfa_node->set_vertex(2);
+            nfa_node->set_final_state({1});
+            nfa_node->add_transition(0, 1, ast_node[0]);
+            nfa_nodes.push(*nfa_node);
+        }
+    }
+
+    return nfa_nodes.top();
+}
 
 AF nfa_to_dfa(AF nfa)
 {
@@ -25,6 +99,9 @@ AF nfa_to_dfa(AF nfa)
         for (char symbol : symbols)
         {
             vector<int> eclosure = nfa.eclosure(nfa.get_transition_vertex(V, symbol));
+
+            if (eclosure.empty())
+                continue;
 
             int to = dfa.get_vertex_index(eclosure);
             if (to == -1)
@@ -56,8 +133,10 @@ bool verify_str(AF dfa, string str)
         {
             if (itr.second[str[start]].empty() == false)
                 S.push_back(itr.first);
-            if (itr.second[str[end - 1]].empty() == false)
-                L.push_back(itr.second[str[end - 1]][0]);
+            if (i && itr.second[str[start - 1]].empty() == false)
+                L.push_back(itr.second[str[start - 1]][0]);
+            else
+                L.push_back(0);
         }
 
         sort(S.begin(), S.end());
@@ -94,7 +173,7 @@ bool verify_str(AF dfa, string str)
     if (!f_state)
         return 0;
 
-    int cur = I.front().front().back();
+    /*int cur = I.front().front().back();
     for (int i = 1; i < n_threads; i++)
     {
         int start = i * round(str.size() * 1.0 / n_threads);
@@ -116,7 +195,7 @@ bool verify_str(AF dfa, string str)
         }
         if (!f_state)
             return 0;
-    }
+    }*/
 
     return 1;
 }
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    AF nfa;
+    /*AF nfa;
     nfa.set_vertex(4);
     nfa.set_final_state({3});
     nfa.add_transition(0, 0, 'a');
@@ -16,22 +16,29 @@ int main()
     nfa.add_transition(1, 3, '^');
     nfa.add_transition(2, 2, 'a');
     nfa.add_transition(2, 2, 'b');
-    nfa.add_transition(2, 3, 'a');
+    nfa.add_transition(2, 3, 'a');*/
 
-    // nfa.print();
+    AF nfa = re_to_nfa("b?c|a*cf");
+    /*nfa.print();
+    auto f_st = nfa.get_final_states();
+    cout << "Final states: ";
+    for (auto x : f_st)
+    {
+        cout << x << ' ';
+    }
+    cout << '\n';*/
 
     AF dfa = nfa_to_dfa(nfa);
-    dfa.print();
-
+    /*dfa.print();
     auto f_st = dfa.get_final_states();
     cout << "Final states: ";
     for (auto x : f_st)
     {
         cout << x << ' ';
     }
-    cout << '\n';
+    cout << '\n';*/
 
-    bool res = verify_str(dfa, "aaaa");
+    bool res = verify_str(dfa, "baaaacf");
 
     cout << "Result of verification: " << res << '\n';
 
